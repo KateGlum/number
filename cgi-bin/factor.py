a4800ff3a4ba4a1eaa61b17e030228bc89caa3ea
kate <gea-81@mail.ru>
2016-04-28 17:45:46 +0300

    test

--- cgi-bin/factor.py
+++ cgi-bin/factor.py
@@ -3,29 +3,31 @@
 
 import cgi
 
+
 print("Content-type: text/html\r\n\r\n")
 print("<html>")
 print("<head><title>Разложение числа на множители</title></head>")
 print("<body>")
 print('<form method="post" action="factor.py">')
 print('</form>')
-form = cgi.FieldStorage()                 # передаем переменную в форму
+form = cgi.FieldStorage()                  # передаем переменную в форму
 if form.getvalue("number"):
-    number = int(form.getvalue("number"))
+    number = int(form.getvalue("number"))  # введенное значение в форму - число
 
-    def factor(number):                   # разложение числа на множители
+    def factor(number):                    # разложение числа на множители
         a = []
         d = 2
-        while d * d <= number:            # проверяем делимость number на
-            if number % d == 0:           # натуральные числа, начиная с 2
-                a.append(d)               # добавляем делитель в список
+        while d * d <= number:             # проверяем делимость number на
+            if number % d == 0:            # натуральные числа, начиная с 2
+                a.append(d)                # добавляем делитель в список
                 number //= d
             else:
                 d += 1
-        if number > 1:                    # продолжаем перебор пока number не будет равно 1
-            a.append(number)              # добавляем number к списку простых делителей
-        return str(a)
+        if number > 1:                     # продолжаем перебор пока number не будет равно 1
+            a.append(number)               # добавляем number к списку простых делителей
+        return str(a)                      # возвращается строка
 
-    print('<h2>' + factor(number) + '<h2><br />') # вывод результата расчета функции
+    print('<h2>' + factor(number) + '<h2><br />')  # вывод результата расчета функции
 print("</body>")
 print("</html>")
+
---
+++ test.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
+
+import unittest
+
+
+def factor(number):
+    a = []
+    d = 2
+    while d * d <= number:
+        if number % d == 0:
+            a.append(d)
+            number //= d
+        else:
+            d += 1
+    if number > 1:
+        a.append(number)
+    return str(a)
+
+
+class Test(unittest.TestCase):
+
+    def test_negativenumber(self):
+        for number in range(0, -42):
+            self.assertFalse(factor(number))
+
+    def test_normnumber(self):
+        self.assertTrue(factor(42))
+
+
+if __name__ == '__main__':
+    unittest.main()
